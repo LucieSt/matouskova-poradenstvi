fecee3b5ae6af6abb4ba1a21e1abe118719b5ae4
Lucie Straková <strakova.91@gmail.com>
2020-05-25 23:37:16 +0200

    work on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,27 @@
   <div class="home-view container">
     <h1>PORADENSKÁ ČINNOST</h1>
     <div class="underline"></div>
+
+    <div class="content">
+      <section class="intro">
+        <img src="./../assets/foto.jpg" alt="profile picture">
+        <div class="intro-text">
+          <p>
+            Nabízím možnost spolupráce provozovatelům potravinářských podniků. Poskytuji pomoc při zavádění
+            <b>systému kritických a kontrolních bodů (HACCP), systémů správné výrobní, hygienické a distribuční
+              praxe (SVP, SHP, SDP)</b> a při implementaci požadavků standardů nad rámec základních právních požadavků.
+          </p>
+          <p>
+            Standardy, s jejichž implementací Vám mohu pomoci:
+          </p>
+          <ul class="intro-ul">
+            <li>IFS Food</li>
+            <li>IFS Logistics</li>
+            <li>BIO certifikace</li>
+          </ul>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
