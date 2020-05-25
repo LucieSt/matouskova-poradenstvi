90b49230aa249c0cfc4c26c68660bd6d3dac374b
Lucie Straková <strakova.91@gmail.com>
2020-05-26 00:28:53 +0200

    homepage finished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,37 @@
             <b>systému kritických a kontrolních bodů (HACCP), systémů správné výrobní, hygienické a distribuční
               praxe (SVP, SHP, SDP)</b> a při implementaci požadavků standardů nad rámec základních právních požadavků.
           </p>
-          <p>
-            Standardy, s jejichž implementací Vám mohu pomoci:
-          </p>
+          <h2>Standardy, s jejichž implementací Vám mohu pomoci:</h2>
           <ul class="intro-ul">
             <li>IFS Food</li>
             <li>IFS Logistics</li>
             <li>BIO certifikace</li>
           </ul>
         </div>
+      </section>
+      <section class="coop">
+        <h2>Jak spolupracuji a co mohu poskytnout?</h2>
+        <ul class="coop-ul">
+          <li>komplexní poradenská pomoc při vypracování dokumentace dle požadavků daného standardu</li>
+          <li>poradenská pomoc při realizaci procesů a činností vyžadovaných daným standardem</li>
+          <li>aktivní vypracování informačních cest (formuláře a jiné záznamníky)</li>
+          <li>aktualizace dokumentace v případě potřeby</li>
+          <li>školení pracovníků ve Vaší provozovně</li>
+          <li>průběžné informování o legislativních změnách a novinkách a  poradenská pomoc při aplikaci
+             relevantních právních požadavků</li>
+          <li>osobní účast při certifikačních auditech</li>
+          <li>poradenská pomoc při nápravě zjištěných neshod</li>
+        </ul>
+      </section>
+      <section class="advice">
+        <p>
+          Pro úspěšnou spolupráci je nutná aktivní účast z Vaší strany, jedině společnou prací je možné se dostat ke
+           správnému výsledku.
+        </p>
+        <p>
+          Pravidelné informování o legislativních změnách a novinkách poskytuji pouze v rámci dlouhodobého poradenství,
+           nikoliv jako samostatnou službu.
+        </p>
       </section>
     </div>
   </div>
