c6f073610fb8ebc0d93139077494b3abeeafc0a6
Lucie Straková <strakova.91@gmail.com>
2020-05-28 01:25:02 +0200

    first finish

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,6 +2,23 @@
   <div class="contact-view container">
     <h1>KONTAKT</h1>
     <div class="underline"></div>
+
+    <div class="content">
+      <section class="contact">
+        <div class="first-part">
+          <p><b>Ing. Veronika Matoušková</b></p>
+          <p>Nad Údolím 126</p>
+          <p>Husinec - Řež</p>
+          <p>250 68</p>
+          <p>Česká republika</p>
+          <p>IČ: 06174698</p>
+        </div>
+        <div class="second-part">
+          <p>kontaktní e-mail:</p>
+          <p><span>kankova.poradenstvi</span><span class="second-span">@gmail.com</span></p>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
--- src/views/References.vue
+++ src/views/References.vue
@@ -2,6 +2,64 @@
   <div class="references-view container">
     <h1>REFERENCE</h1>
     <div class="underline"></div>
+
+    <div class="content">
+      <section class="reference-container">
+        <div class="logo">
+          <span></span><img class="made-group" src="./../assets/madegroup.png" alt="made-group-logo">
+        </div>
+        <div class="text">
+          <p class="first-p">MADE GROUP, a.s.</p>
+          <p>IFS Food, IFS Logistics, HACCP, SVP/SDP/SHP</p>
+        </div>
+      </section>
+      <section class="reference-container">
+        <div class="logo">
+          <span></span><img class="gornicky" src="./../assets/gornicky.png" alt="gornicky-logo">
+        </div>
+        <div class="text">
+          <p class="first-p">GORNICKY, s.r.o.</p>
+          <p>IFS Logistics, HACCP, SVP/SDP/SHP</p>
+        </div>
+      </section>
+      <section class="reference-container">
+        <div class="logo">
+          <span></span><img class="stevikom" src="./../assets/stevikom.jpg" alt="stevikom-logo">
+        </div>
+        <div class="text text-margin">
+          <p class="first-p">STEVIKOM, s.r.o.</p>
+          <p>IFS Food, HACCP, SVP/SDP/SHP, BIO</p>
+        </div>
+      </section>
+      <section class="reference-container">
+        <div class="logo">
+          <span></span><img class="ilovehummus" src="./../assets/ilovehummus.png" alt="ilovehummus-logo">
+        </div>
+        <div class="text text-margin">
+          <p class="first-p">I LOVE HUMMUS, s.r.o.</p>
+          <p>IFS Food, HACCP, SVP/SDP/SHP, BIO</p>
+        </div>
+      </section>
+      <section class="reference-container">
+        <div class="logo">
+          <span></span><img class="fishmarket" src="./../assets/fishmarket.png" alt="fishmarket-logo">
+        </div>
+        <div class="text text-margin">
+          <p class="first-p">FISH MARKET, a.s.</p>
+          <p>IFS Food, HACCP, SVP/SDP/SHP</p>
+        </div>
+      </section>
+      <section class="reference-container">
+        <div class="logo">
+          <span></span><img class="rybarstvi-chlumec" src="./../assets/rybarstvi-chlumec.jpg" alt="rybarstvi-chlumec-logo">
+        </div>
+        <div class="text text-margin">
+          <p class="first-p">RYBÁŘSTVÍ CHLUMEC NAD CIDLINOU, a.s.</p>
+          <p>IFS Food, HACCP, SVP/SDP/SHP</p>
+        </div>
+      </section>
+    </div>
+
   </div>
 </template>
 
